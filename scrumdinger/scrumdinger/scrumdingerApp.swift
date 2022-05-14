410717c6635bab97d2ced7856f55a40821d5443f
Malayan Colleges Mindanao <>
2022-05-14 15:09:57 +0800

    Initial commit

---
+++ scrumdinger/scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Malayan Colleges Mindanao on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
